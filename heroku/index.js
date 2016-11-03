7ced6092d25e0dadc684ae91c6846eff4a7ce9e8
hatemLW <hatem@logicsware.com>
2016-11-03 15:44:56 +0200

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -4,7 +4,7 @@
  *
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
- */
+
 
 var bodyParser = require('body-parser');
 var express = require('express');
@@ -30,7 +30,7 @@ app.get(['/facebook', '/instagram'], function(req, res) {
     res.send(req.param('hub.challenge'));
   } else {
     res.sendStatus(400);
-  }
+  } 
 });
 
 app.post('/facebook', function(req, res) {
@@ -63,3 +63,67 @@ app.post('/instagram', function(req, res) {
 });
 
 app.listen();
+*/
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
+
+app.use(bodyParser.json());
+
+app.set('port', (process.env.PORT || 5000));
+app.set('verify_token', (process.env.VERIFY_TOKEN || 'TEST'));
+app.set('page_access_token', (process.env.PAGE_ACCESS_TOKEN || 'NULL'));
+
+app.get('/', function (req, res) {
+        res.send('It Works! Follow FB Instructions to activate.');
+});
+
+app.get('/facebook', function (req, res) {
+    if (req.query['hub.verify_token'] === app.get('verify_token')) {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Error, wrong validation token');
+    }
+});
+
+app.post('/facebook/', function (req, res) {
+    console.log (req.body);
+    messaging_events = req.body.entry[0].messaging;
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i];
+        sender = event.sender.id;
+        if (event.message && event.message.text) {
+            text = event.message.text;
+            // Your Logic Replaces the following Line
+            sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+        }
+    }
+    res.sendStatus(200);
+});
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:app.get('page_access_token')},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+}
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
