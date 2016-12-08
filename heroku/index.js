408550544c55c7cd3f81a99b15a638efe4e973c9
hatemLW <hatem@logicsware.com>
2016-12-08 16:17:14 +0200

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -17,13 +17,13 @@ app.get('/', function (req, res) {
                 var request = require('request');
 request('http://www.google.com', function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body) // Show the HTML for the Google homepage. 
+    console.log(body); // Show the HTML for the Google homepage. 
   }
 })
         }
         catch(e)
         {
-                console.log(e)
+                console.log(e);
         }
 });
 
